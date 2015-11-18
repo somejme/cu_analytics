52cedc9f2b57d14a75772cd80f9021f3f7a8851a
sxdas <sayooj.das@gmail.com>
2015-11-18 00:23:40 -0700

    gbm

---
+++ Capstone Project/Code.R
@@ -0,0 +1,62 @@
+library(ggplot2)
+library(rpart)
+library(rattle)
+library(rpart.plot)
+library(RColorBrewer)
+library(gbm)
+
+train <- read.csv("~/GitHub/cu_analytics/Capstone Project/train.csv")
+test <- read.csv("~/GitHub/cu_analytics/Capstone Project/test.csv")
+
+PREDICT = TRUE
+data=train
+if(PREDICT){data=test}
+
+set.seed(420)
+
+########### Feature Engineering #####################
+data$Name = as.character(data$Name)
+data$Ticket = as.character(data$Ticket)
+data$Cabin = as.character(data$Cabin)
+data$Child <- 0
+data$Child[data$Age < 18] <- 1
+data$Fare2 <- 'high'
+data$Fare2[data$Fare < 100 & data$Fare >= 40] <- 'mid'
+data$Fare2[data$Fare < 40 & data$Fare >= 10] <- 'low'
+data$Fare2[data$Fare < 10] <- 'vlow'
+data$Fare2 = as.factor(data$Fare2)
+Agefit <- rpart(Age ~ Pclass + Sex + SibSp + Parch + Fare2 + Embarked, data=data[!is.na(data$Age),], method="anova")
+data$Age[is.na(data$Age)] <- predict(Agefit, data[is.na(data$Age),])
+
+########### EDA ############
+# if(!PREDICT){
+#   table(train$Survived)
+#   prop.table(table(train$Survived))
+#   table(train$Sex)
+#   prop.table(table(train$Sex, train$Survived),2)
+#   summary(train$Age)
+#   summary(train$Fare)
+#   aggregate(Survived ~ Fare2 + Pclass + Sex, data=train, FUN=function(x) {sum(x)/length(x)})
+#   fancyRpartPlot(Agefit)
+# }
+
+?hist
+hist(data[data$Fare<50,]$Fare,breaks = 100)
+# str(data)
+str(data)
+table(data$Parch)
+
+
+if(!PREDICT){
+  fit = gbm(Survived ~ Pclass + Sex + Fare2 + Age + SibSp + Parch + Embarked,
+            data=data, distribution = "bernoulli", n.trees = 8000, interaction.depth = 4, shrinkage = 0.0005, n.cores = 2)
+  gbm.perf(fit,plot.it = FALSE)
+}
+
+
+if(PREDICT){
+  Prediction <- predict(fit, data, n.trees=6102, type="response")
+  Prediction = ifelse(Prediction<0.45,0,1)
+  submit <- data.frame(PassengerId = data$PassengerId, Survived = Prediction)
+  write.csv(submit, file = "submission.csv", row.names = FALSE)
+          }
---
+++ Capstone Project/Interactive_Demo_Project.md
@@ -0,0 +1,36 @@
+Titanic : Predicting Survivors
+========================================================
+author: Courtney / Katie/ Jamie / Sayooj
+date: 
+
+Goal
+========================================================
+
+Predict survivors from the Titanic tragedy
+
+- Binary Classification problem : Survived or NOT
+- Reasonably clean dataset
+- Can we predict 100% ? 
+
+Slide With Code
+========================================================
+
+
+```r
+summary(cars)
+```
+
+```
+     speed           dist       
+ Min.   : 4.0   Min.   :  2.00  
+ 1st Qu.:12.0   1st Qu.: 26.00  
+ Median :15.0   Median : 36.00  
+ Mean   :15.4   Mean   : 42.98  
+ 3rd Qu.:19.0   3rd Qu.: 56.00  
+ Max.   :25.0   Max.   :120.00  
+```
+
+EDA
+========================================================
+
+![plot of chunk unnamed-chunk-2](Interactive_Demo_Project-figure/unnamed-chunk-2-1.png) 
